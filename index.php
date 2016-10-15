a0baa3ca71e890f30578029460115d289286ffe9
Jakub Zaba <kuba.zaba@gmail.com>
2016-10-15 11:20:04 +0200

    bank proxy

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace FKolonial;
+namespace Bank;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -32,10 +32,13 @@ $guzzle = new Client(['verify' => false]);
 $bankUrl = $_POST['bank_url'];
 $apiKey = $_POST['api_key'];
 $accessToken = $_POST['access_token'];
+/*$bankUrl = 'https://www.csas.cz/webapi/api/v1/netbanking/my/accounts/CZ5608000000002326573123/transactions?dateStart=2016-09-15T00:00:00Z&dateEnd=2016-10-15T00:00:00Z';
+$apiKey = '0bca73a4-0ebb-4837-a841-7dcb189e9c02';
+$accessToken = 'demo_b8d3fb54a86b63641727eba34fd638ef';*/
 
 try {
-	$response = $guzzle->post($bankUrl, [
-		'json' => [
+	$response = $guzzle->get($bankUrl, [
+		'headers' => [
 			'WEB-API-key' => $apiKey,
             'Authorization' => 'Bearer ' . $accessToken,
 		],
@@ -44,11 +47,10 @@ try {
 	return $fail($e->getMessage(), 500);
 }
 
-echo $response->getBody()->getContents();
+$body = \json_decode($response->getBody()->getContents());
 
-/*$transactions = \json_decode($response->getBody()->getContents())->transactions;
-
-foreach ($transactions as $transaction) {
-	echo \json_encode($transaction->description, JSON_UNESCAPED_UNICODE);
-}*/
+foreach ($body->transactions as &$transaction) {
+	$transaction->test = 'test';
+}
+echo \json_encode($body, JSON_UNESCAPED_UNICODE);
 return;
