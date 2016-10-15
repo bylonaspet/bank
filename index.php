5496a10c5965117f8e6c071b111cc37529a4c4ee
Jakub Zaba <kuba.zaba@gmail.com>
2016-10-15 11:35:36 +0200

    kolonial tr

--- index.php
+++ index.php
@@ -15,26 +15,34 @@ $fail = function ($reason, $code) {
 	echo \json_encode((object) ['error' => (string) $reason], JSON_UNESCAPED_UNICODE);
 };
 
-$requiredQueryArgs = [
-	'bank_url',
-	'api_key',
-	'access_token',
-];
+if (php_sapi_name() !== "cli") {
+	$requiredQueryArgs = [
+		'bank_url',
+		'api_key',
+		'access_token',
+	];
 
-foreach ($requiredQueryArgs as $arg) {
-	if (!array_key_exists($arg, $_POST)) {
-		return $fail(sprintf('Missing parameter %s', $arg), 404);
+	foreach ($requiredQueryArgs as $arg) {
+		if (!array_key_exists($arg, $_POST)) {
+			return $fail(sprintf('Missing parameter %s', $arg), 404);
+		}
 	}
 }
 
+
 $guzzle = new Client(['verify' => false]);
 
-$bankUrl = $_POST['bank_url'];
-$apiKey = $_POST['api_key'];
-$accessToken = $_POST['access_token'];
-/*$bankUrl = 'https://www.csas.cz/webapi/api/v1/netbanking/my/accounts/CZ5608000000002326573123/transactions?dateStart=2016-09-15T00:00:00Z&dateEnd=2016-10-15T00:00:00Z';
-$apiKey = '0bca73a4-0ebb-4837-a841-7dcb189e9c02';
-$accessToken = 'demo_b8d3fb54a86b63641727eba34fd638ef';*/
+if (php_sapi_name() !== "cli") {
+	$bankUrl = $_POST['bank_url'];
+	$apiKey = $_POST['api_key'];
+	$accessToken = $_POST['access_token'];
+} else {
+	$bankUrl = 'https://www.csas.cz/webapi/api/v1/netbanking/my/accounts/CZ5608000000002326573123/transactions?dateStart=2016-09-15T00:00:00Z&dateEnd=2016-10-15T00:00:00Z';
+	$apiKey = '0bca73a4-0ebb-4837-a841-7dcb189e9c02';
+	$accessToken = 'demo_b8d3fb54a86b63641727eba34fd638ef';
+
+}
+
 
 try {
 	$response = $guzzle->get($bankUrl, [
@@ -50,7 +58,27 @@ try {
 $body = \json_decode($response->getBody()->getContents());
 
 foreach ($body->transactions as &$transaction) {
-	$transaction->test = 'test';
+	$transaction->enhanced = null;
 }
+
+$knTransaction = array_merge([], $transaction);
+$knTransaction->description = 'Nákup na Koloniál.cz';
+$knTransaction->amount->value = 135;
+$knTransaction->enhanced = [
+	'type' => 'kolonial',
+];
+
+$body->transactions[] = $knTransaction;
+
+
+$arr = &$body->transactions;
+// rotate
+$keys = array_keys($arr);
+$val = $body->transactions[$keys[0]];
+unset($arr[$keys[0]]);
+$arr[$keys[0]] = $val;
+
+
 echo \json_encode($body, JSON_UNESCAPED_UNICODE);
+
 return;
